673f1ea1b84c5cf6dbefd27b75416e1f5222aa3b
JefBronze <jefersonbronze@gmail.com>
2023-08-25 20:01:55 +0000

    Output.log

--- app/routes.py
+++ app/routes.py
@@ -20,16 +20,31 @@ def index():
     chat_session = _get_user_session()
     return render_template("chat.html", conversation=chat_session.get_messages())
 
+# @app.route('/chat', methods=['POST'])
+# def chat():
+#     print("Chat endpoint called") # Debug line
+#     message: str = request.json['message']
+#     print("User message:", message) # Debug line
+#     chat_session = _get_user_session()
+#     chatgpt_message = chat_session.get_chatgpt_response(message)
+#     return jsonify({"message": chatgpt_message})
+
 @app.route('/chat', methods=['POST'])
-def chat():
-    print("Chat endpoint called") # Debug line
+def chat_endpoint():  # Rename this to avoid conflicts
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    print(f"Client IP Address: {client_ip}")
+    print("Chat endpoint called")
     message: str = request.json['message']
-    print("User message:", message) # Debug line
+    print("User message:", message)
     chat_session = _get_user_session()
     chatgpt_message = chat_session.get_chatgpt_response(message)
     return jsonify({"message": chatgpt_message})
-
-
+    user_agent = request.headers.get('User-Agent')
+    print(f"User-Agent: {user_agent}")
+    cookies = request.cookies
+    print(f"Cookies: {cookies}")
+    session_data = session.get("some_key")
+    print(f"Session data: {session_data}")
 
 def _get_user_session() -> ChatSession:
     chat_session_id = session.get("chat_session_id")
